14e4260ae892c7b1ba346383867ce250d911e6b3
rawfoon <mgrawfoon@gmail.com>
2022-11-09 22:09:43 +0600

    review posted finished

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -21,6 +21,35 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
 
     const serviceCollection = client.db('globeRouteTravels').collection('services')
+    const reviewCollection = client.db('globeRouteTravels').collection('reviews')
+
+
+
+    app.get('/homeservices', async (req, res)=>{
+        const query = {}
+        const cursor = serviceCollection.find(query)
+
+        const services = await cursor.limit(3).toArray()
+        res.send(services)
+    })
+
+
+    app.get('/services', async (req, res)=>{
+        const query = {}
+        const cursor = serviceCollection.find(query)
+
+        const services = await cursor.toArray()
+        res.send(services)
+    })
+
+    app.get('/services/:id', async (req, res)=>{
+        const id = req.params.id
+        const query = {_id: ObjectId(id)}
+        const cursor = serviceCollection.find(query)
+
+        const services = await cursor.toArray()
+        res.send(services)
+    })
 
 
 
@@ -29,6 +58,15 @@ async function run(){
         const addServices = req.body
         // console.log(addServices);
         const result = await serviceCollection.insertOne(addServices)
+        res.send(result)
+    })
+
+
+    // create reviews
+    app.post('/reviews', async(req, res)=>{
+        const addReviews = req.body
+        // console.log(addReviews);
+        const result = await reviewCollection.insertOne(addReviews)
         res.send(result)
     })
 
